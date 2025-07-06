c24d53c896cb05144c9a9fab3b99c5f01babe79c
jakkapan-a <jakkapan.attala@gmail.com>
2025-07-06 12:16:19 +0700

    Add db connect

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,10 +4,17 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/jakkapan-a/fiber-next-ecommerce/config"
+	"github.com/jakkapan-a/fiber-next-ecommerce/internal/adapters/db"
 )
 
 func main() { // Initialize the API server
+
 	app := fiber.New()
+
+	cfg := config.LoadConfig() // Connect to the database using the configuration
+
+	db.Connect(cfg) // Connect to the database
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
